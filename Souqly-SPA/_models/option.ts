9e975444cda1419a99449697056eaa5462a13c42
Eng-Ahmed Hussen <ahmedhussen.fcaih@gmail.com>
2021-06-03 21:25:41 +0200

    LastComAh

--- Souqly-SPA/_models/option.ts
+++ Souqly-SPA/_models/option.ts
@@ -1,16 +1,10 @@
-export interface Option {
-    id: number
+import { Product } from "./Product";
+import { ProductOptionCart } from "./ProductOptionCart";
 
-    code: string
-  
-    stockIn: string
-  
-    name: string
-  
-    itemPrice: number
-  
-    availableOptions: string
-  
-    productId: number
+export class Option {
+    constructor(public id?:number, public code?:string, public stockIn?:number, public name?:string,
+        public itemPrice?:number, public availableOptions?:string, public product?:Product, 
+        public productId?:number, public productOptionCart?:ProductOptionCart[]){
+
+    }
 }
-
